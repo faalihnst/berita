ef7fb307669cd59db866bab8f9e8d38cf103004b
faalihnst <faalihnst@gmail.com>
2020-06-04 02:32:05 +0700

    change ui, search by category

---
+++ .history/src/App_20200604003205.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import qs from 'qs';
+import './assets/style.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { MDBCol, MDBFormInline, MDBIcon } from "mdbreact";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Homepage from "./pages/homepage";
+// import Login from "./Pages/login";
+// import Skripsi from "./Pages/skripsi";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={Homepage}></Route>
+        {/* <Route exact path="/Login" component={Login}></Route>
+        <Route exact path="/Skripsi" component={Skripsi}></Route> */}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
